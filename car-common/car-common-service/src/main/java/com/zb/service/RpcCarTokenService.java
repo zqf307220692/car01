645f5103559d1478a53114e84ba9f3583c75f819
zqf <307220692@qq.com>
2019-03-28 10:54:34 +0800

    该注释

--- car-common/car-common-service/src/main/java/com/zb/service/RpcCarTokenService.java
+++ car-common/car-common-service/src/main/java/com/zb/service/RpcCarTokenService.java
@@ -5,9 +5,10 @@ import com.zb.entity.User;
 
 public interface RpcCarTokenService {
 
+    //生成token
     public String gengeratToken(String userAgentString, User user);
 
-
+    //保存token
     public void saveToken(String token,User user);
 
 }
