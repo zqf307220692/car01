ba2f58747069d0f9096613e1edded174f4076b8a
zn <tencentlol@163.com>
2019-03-28 09:50:06 +0800

    6600

--- car-common/car-common-mapper/src/main/java/com/zb/mapper/AreaMapper.java
+++ car-common/car-common-mapper/src/main/java/com/zb/mapper/AreaMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AreaMapper {
-  //66677
+  //6600
 	public Area getAreaById(@Param(value = "id") Long id)throws Exception;
 
 	public List<Area>	getAreaListByMap(Map<String,Object> param)throws Exception;
